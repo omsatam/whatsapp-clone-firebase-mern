c5ae0ab014829c9a68f9b143318e0db0c298367d
omsatam <62635289+omsatam@users.noreply.github.com>
2021-04-30 21:52:54 +0530

    Add files via upload

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,89 @@
+//importing
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: "1195757",
+  key: "c1e02d80a42c4e584b8a",
+  secret: "d912e357525825ec6f01",
+  cluster: "ap2",
+  useTLS: true,
+});
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   next();
+// });
+
+//dbconfig
+const connection_url =
+  "mongodb+srv://admin:NIr6oDPOBedlkarn@cluster0.6nqxo.mongodb.net/whatsapp-mern-db?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        recieved: messageDetails.recieved,
+      });
+    } else {
+      console.log("Error triggering pusher");
+    }
+  });
+});
+
+//api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message created: \n ${data}`);
+    }
+  });
+});
+
+//listener
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
+// admin:NIr6oDPOBedlkarn
